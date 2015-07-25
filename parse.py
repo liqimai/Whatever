87c267b711289490e0047a316847781512e1921b
Liqimai <liqimai@outlook.com>
2015-07-26 00:59:22 +0800

    Remove punctuation during normalization

--- parse.py
+++ parse.py
@@ -12,22 +12,23 @@ class Parser(object):
             if word.isalpha():
                 self.__stopwords.append(self.__stemmer.stem(word.lower(),0,len(word)-1))
         self.__stopwords = set(self.__stopwords)
-    # __punctuation = [
-    # '!','@','#','$','%','^','&','*','(',')','_','+','-','=','~','`',
-    # ',','.','/',';','\'','[',']','\\','<','>','?',':','\"','{','}','|',
-    # '，','。','、','；','‘','’','【','】','、','《','》','？','：','“',
-    # '”','｛','｝','|','￥','！','…','（','）','——','-']
+    __punctuation = set([
+    u'!',u'@',u'#',u'$',u'%',u'^',u'&',u'*',u'(',u')',u'_',u'+',u'-',u'=',u'~',u'`',
+    u',',u'.',u'/',u';',u'\'',u'[',u']',u'\\',u'<',u'>',u'?',u':',u'\"',u'{',u'}',u'|'])#,
+    # u'，',u'。',u'、',u'；',u'‘',u'’',u'【',u'】',u'、',u'《',u'》',u'？',u'：',u'“',u
+    # u'”',u'｛',u'｝',u'|',u'￥',u'！',u'…',u'（',u'）',u'——',u'-'])
     def normalize(self,str):
         a = []
         for word in jieba.cut(str):
             word = word.strip()
-            if word: #and word not in self.__punctuation:
+            if word and word not in self.__punctuation: #and word not in self.__punctuation:
                 if word.isalpha():
                     word = self.__stemmer.stem(word.lower(),0,len(word)-1)
                     if word not in self.__stopwords:
                         a.append(word)
                 else:
-                    a.append(word)
+                    if not word.isdigit():
+                        a.append(word)
         return a
 
 if __name__ == '__main__':
